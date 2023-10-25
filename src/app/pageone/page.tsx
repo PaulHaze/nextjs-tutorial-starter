3437b23730d7b2dbe6b6d0e07a47f405ffff9123
PaulHaze <dev@artemist.com.au>
2023-10-25 12:56:39 +1100

    general tidy up

--- src/app/pageone/page.tsx
+++ src/app/pageone/page.tsx
@@ -1,6 +1,6 @@
 export default function PageOne() {
   return (
-    <div className="flex-center h-screen bg-gray-300">
+    <div className="flex-center h-screen">
       <h4>Page One Placeholder</h4>
     </div>
   );
